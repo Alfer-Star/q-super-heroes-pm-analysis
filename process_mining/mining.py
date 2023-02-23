ce769506411766e626f9b8fada7e6d3346e1fae6
Alfer-Star <alferman@outlook.de>
2023-02-23 10:55:15 +0100

    Proces Mining Analyse

--- process_mining/mining.py
+++ process_mining/mining.py
@@ -1,6 +1,10 @@
 import pm4py
 
+from pm4py.objects.petri_net.utils import petri_utils
+
 from mining_helper import *
+
+from os import mkdir
 
 #Generate BPMN Model from example xes files
 if __name__ == "__main__":
@@ -9,15 +13,30 @@ if __name__ == "__main__":
     
     finalModelEventLogPath ='C:/Users/alfer/Desktop/Dev Projects/ba-obs-pm/xes_export/discovery_with_new_classifier_utc_14_02.xes'
     
-    log = pm4py.read_xes('C:/Users/alfer/Desktop/Dev Projects/ba-obs-pm/xes_export/example.xes')
+    log = pm4py.read_xes('C:/Users/alfer/Desktop/Dev Projects/ba-obs-pm/xes_export/discovery2_faultfree.xes')
     
-    #faulty_log = pm4py.read_xes('C:/Users/alfer/Desktop/Dev Projects/ba-obs-pm/xes_export/example.xes')
+    iterI= True
+    iterII= iterI and True
+    iterIII= iterII and True
+    iterIV = True
     
-    filtered_log = pm4py.filter_event_attribute_values(log, 'lifecycle:transition', ['complete'], level='event')
-    filtered_log = pm4py.filter_start_activities(filtered_log, ['/api/fights', '/api/fights/randomfighters'])
-    filtered_log = pm4py.filter_event_attribute_values(filtered_log, 'lifecycle:transition')
-    #filtered_log = log
+    alphaAlg = False
     
+    filtered_log = log
+    if iterI and not iterIV:
+        filtered_log =  pm4py.filter_event_attribute_values(filtered_log, 'lifecycle:transition', ['start'], level='event', retain=True)
+    if iterII:
+        filtered_log = pm4py.filter_event_attribute_values(filtered_log, 'concept:name', ['/api/fights', '/api/fights/randomfighters'])
+    if iterIII:
+        #filtered_log = pm4py.filter_variants_top_k(filtered_log, 10)
+        filtered_log = pm4py.filter_directly_follows_relation(filtered_log, [('/api/heroes/random','HeroService.findRandomHero'),('/api/villain/random', 'VillainService.findRandomVillain'), ('/api/fights', 'fights send') ])
+    #filtered_log = pm4py.filter_start_activities(filtered_log, ['/api/fights', '/api/fights/randomfighters'])
+    
+    if alphaAlg:
+        # Model is really bad...
+        # does not show the two branches :(
+        petri_net= discoverPetriNetAlphaPlus(filtered_log, True)
+        
     bpmn_model = createBpmnModelFromLog(filtered_log, True)
     
     # keine neuen Informationen...
@@ -28,27 +47,78 @@ if __name__ == "__main__":
     
     #processMapPerf = discoverProcessDFGPerf(filtered_log, True)
     
-    process_tree = discoverProcessTree(filtered_log)
+    process_tree = discoverProcessTreeInductive(filtered_log)
     
-    petri_net, im, fm = discoverPetriNet(filtered_log)
+    petri_net, im, fm = discoverPetriNet(filtered_log, True)
+    if False:
+        transition = petri_utils.get_transition_by_name(petri_net, 'VillainService.findRandomVillain')
+        print(transition)
     
-    #rebuilding petri_net?
+    ## Model Quality 
     
-    # rebuilding event log?
+    printModelQ = False
+    
+    pathModelQ = './process_mining/discovery'
+    try:
+        os.makedirs(pathModelQ)
+    except FileExistsError:
+    # directory already exists
+        pass
     
     fitness = pm4py.fitness_token_based_replay(log,petri_net, im, fm)
-    writeJsonFile(fitness, "./process_mining/checking_traces/log_fitness")
+    writeJsonFile(fitness, pathModelQ + "/log_fitness")
     
     conformance_diagnostics = pm4py.conformance_diagnostics_token_based_replay(log,petri_net, im, fm)
     unfitting_traces = [trace for trace in conformance_diagnostics if not trace.get('trace_is_fit')]
-    print('unfitting Traces: ' + str(len(unfitting_traces)))
-    for trace_diagnostic in unfitting_traces: 
-        print(trace_diagnostic)
-        
-    
-    exportConformanceDiagnosisAsJson(conformance_diagnostics, "./process_mining/checking_traces")
+    if printModelQ:
+        print('unfitting Traces: ' + str(len(unfitting_traces)))
+        for trace_diagnostic in unfitting_traces: 
+            print(trace_diagnostic)
+         
+    (unfitting_traces_discovery, fitting_traces_discovery) = exportConformanceDiagnosisAsJson(conformance_diagnostics, pathModelQ)
 
+    ## Conformance Checking
     
+    faulty_log = pm4py.read_xes('C:/Users/alfer/Desktop/Dev Projects/ba-obs-pm/xes_export/example.xes')
+    
+    if iterI and not iterIV:
+        faulty_log =  pm4py.filter_event_attribute_values(faulty_log, 'lifecycle:transition', ['start'], level='event', retain=True)
+    if iterII:
+        faulty_log = pm4py.filter_event_attribute_values(faulty_log, 'concept:name', ['/api/fights', '/api/fights/randomfighters'])
+    if iterIII:
+        #filtered_log = pm4py.filter_variants_top_k(filtered_log, 10)
+        faulty_log = pm4py.filter_directly_follows_relation(faulty_log, [('/api/heroes/random','HeroService.findRandomHero'),('/api/villain/random', 'VillainService.findRandomVillain'), ('/api/fights', 'fights send') ])
+    #filtered_log = pm4py.filter_start_activities(filtered_log, ['/api/fights', '/api/fights/randomfighters'])
+    
+    
+    printConfCh = False
+    
+    pathConfCh = './process_mining/conf_checking'
+    try:
+        os.makedirs(pathConfCh)
+    except FileExistsError:
+    # directory already exists
+        pass
+    
+    fitness = pm4py.fitness_token_based_replay(faulty_log,petri_net, im, fm)
+    writeJsonFile(fitness, pathConfCh + "/log_fitness")
+    
+    conformance_diagnostics = pm4py.conformance_diagnostics_token_based_replay(faulty_log,petri_net, im, fm)
+    unfitting_traces = [trace for trace in conformance_diagnostics if not trace.get('trace_is_fit')]
+    if printConfCh:
+        print('unfitting Traces: ' + str(len(unfitting_traces)))
+        for trace_diagnostic in unfitting_traces: 
+            print(trace_diagnostic)
+        
+    (unfitting_traces, fitting_traces) = exportConformanceDiagnosisAsJson(conformance_diagnostics, pathConfCh)
+    
+    # Running Script Interactively: exec(open(mining.py).read())
+    
+    
+    faultList = openCSV('faultcsv.csv')
+    vFaults, fsFaults, fuFault = sortFaultList(faultList)
+    
+    print('IAm Ready to use')
     
     
     
--- process_mining/mining_helper.py
+++ process_mining/mining_helper.py
@@ -11,15 +11,33 @@ def createBpmnModelFromLog(log, view=False):
     if view:
         pm4py.view_bpmn(process_model)
     return process_model
+
+def createBpmnModelFromLog(log, view=False):
+    process_model = pm4py.discover_bpmn_inductive(log)
+    if view:
+        pm4py.view_bpmn(process_model)
+    return process_model
     
-def discoverProcessTree(log, view = False):
+def discoverProcessTreeInductive(log, view = False):
     process_tree = pm4py.discover_process_tree_inductive(log)
+    if view:
+        pm4py.view_process_tree(process_tree)
+    return process_tree
+
+def discoverProcessTree(log, view = False):
+    process_tree = pm4py.discover_prefix_tree(log)
     if view:
         pm4py.view_process_tree(process_tree)
     return process_tree
 
 def discoverPetriNet(log, view = False):
     net, im, fm  = pm4py.discover_petri_net_inductive(log)
+    if view:
+        pm4py.view_petri_net(net)
+    return net, im, fm
+
+def discoverPetriNetAlphaPlus(log, view = False):
+    net, im, fm  = pm4py.discover_petri_net_alpha_plus(log)
     if view:
         pm4py.view_petri_net(net)
     return net, im, fm
@@ -66,8 +84,23 @@ def exportConformanceDiagnosisAsJson(result: list, pathToDirectory: str):
     for index, uTrace in enumerate(fitting_traces):
         writeJsonFile(str(uTrace), fittingDirectory+'/trace_'+str(index))
         
+    return unfitting_traces, fitting_traces
+        
 def writeJsonFile(content: str, filePath: str):
     file = open(filePath+'.json', 'w+')
     jsonStr = json.dumps(content)
     file.write(jsonStr)
     file.close()
+    
+import csv
+def openCSV(path):
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data
+
+def sortFaultList(faultList: list):
+    vFaults = [fault for fault in faultList if str(fault[0]).startswith('VillianFault')]
+    fSwitchedFault = [fault for fault in faultList if str(fault[0]).startswith('fighterSwitched')]
+    fUndefinedFault = [fault for fault in faultList if str(fault[0]).startswith('fighterUndefined')]
+    return vFaults, fSwitchedFault, fUndefinedFault
